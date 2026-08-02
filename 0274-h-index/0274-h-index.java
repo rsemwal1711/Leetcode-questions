026856a1978c7180c5435f0d03a24b24206eb8cd
Rajat Semwal <rsemwal1711@gmail.com>
2026-08-02 16:26:57 +0530

    Time: 5 ms (73.27%), Space: 43.9 MB (5.48%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int ans = 0;
+        int n = citations.length;
+        for(int i=0;i<n;i++){
+            if(citations[i] >= n-i){
+                ans = Math.max(ans, n-i);
+            }
+        }
+        return ans;
+    }
+}
