524ab58a517e3e27cf4d5e3f2fe0d55fc733ddae
Rajat Semwal <rsemwal1711@gmail.com>
2026-08-18 10:19:37 +0530

    Time: 1 ms (14.3%), Space: 42.1 MB (66.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Memoization : 
+    /*
     public int solve(int m, int n, int[][] dp){
         if(m == 0 && n == 0) return dp[0][0] = 1;
         if(m < 0 || n < 0) return 0;
@@ -7,11 +9,30 @@ class Solution {
         int left = solve(m, n-1, dp);
         return dp[m][n] = up + left;
     }
+    */
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
+        // Memoization : 
+        /*
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
         return solve(m-1, n-1, dp);
+        */
+
+        //Tabulation : 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                else{
+                    int up = (i > 0) ? dp[i-1][j] : 0;
+                    int left = (j > 0) ? dp[i][j-1] : 0;
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+
+        // Space Optimization
     }
 }
