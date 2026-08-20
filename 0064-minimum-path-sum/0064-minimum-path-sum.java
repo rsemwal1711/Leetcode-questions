0ffe02f218bcdde12baaf64383fa0ab513bf5bba
Rajat Semwal <rsemwal1711@gmail.com>
2026-08-20 10:08:41 +0530

    Time: 4 ms (61.21%), Space: 50 MB (91.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -2,6 +2,8 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        /*
+        TABULATION : 
         int[][] dp = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -19,5 +21,23 @@ class Solution {
             }
         }
         return dp[n-1][m-1];
+        */
+        
+        // SPACE OPTIMIZATION : 
+        int[] prev = new int[m];
+        for(int j=0;j<m;j++){
+            if(j == 0) prev[0] = grid[0][j];
+            else prev[j] = prev[j-1] + grid[0][j];
+        }
+        for(int i=1;i<n;i++){
+            int[] curr = new int[m];
+            for(int j=0;j<m;j++){
+                int down = prev[j];
+                int right = (j > 0) ? curr[j-1] : (int) 1e9;
+                curr[j] = grid[i][j] + Math.min(down, right);
+            }
+            prev = curr;
+        }
+        return prev[m-1];
     }
 }
