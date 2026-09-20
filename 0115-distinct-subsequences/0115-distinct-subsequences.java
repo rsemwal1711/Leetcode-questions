f28e04cdcdab723ee020ed12c607a532de37ef1a
Rajat Semwal <rsemwal1711@gmail.com>
2026-09-20 09:04:02 +0530

    Time: 17 ms (63.79%), Space: 54.5 MB (23.8%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int func(String s, String t, int i, int j, int[][] dp){
+        if(j < 0) return 1;
+        if(i < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = func(s, t, i-1, j, dp) + func(s, t, i-1, j-1, dp);
+        }
+        else{
+            return dp[i][j] = func(s, t, i-1, j, dp);
+        }
+    }
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n][m];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return func(s, t, n-1, m-1, dp);
+    }
+}
